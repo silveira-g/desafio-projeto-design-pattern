c638fc0e818ffd41071ba91c9f4db426ed9d2638
silveira-g <silveira.alves.guilherme@gmail.com>
2022-11-29 18:59:04 -0300

    Commit inicial

---
+++ src/main/java/dio/desafioprojetodesignpattern/DesafioProjetoDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package dio.desafioprojetodesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/desafioprojetodesignpattern/DesafioProjetoDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafioprojetodesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
